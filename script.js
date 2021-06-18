8a1a16371e04a455c414d836c3294e60eb6a83b4
Aashiq Abrahams <aashiqabrahams44@gmail.com>
2021-06-19 00:13:03 +0200

    level 100 need to add link

--- script.js
+++ script.js
@@ -4,9 +4,23 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+ function makePageForEpisodes(episodeList) {
+ const rootElem = document.getElementById("root");
+
+   episodeList.forEach((episode) => {
+    let tvShowName = document.createElement("h1");
+    tvShowName.innerText = `${episode.name} S${episode.season.toString().padStart(2, '0')}E${episode.number.toString().padStart(2, '0')}`;
+   rootElem.appendChild(tvShowName);
+   let tvShowImage = document.createElement("img");
+   tvShowImage.src = episode.image.medium;
+   tvShowName.appendChild(tvShowImage);
+   let tvShowSummary = document.createElement("p");
+   tvShowSummary.innerHTML = episode.summary;
+   tvShowName.appendChild(tvShowSummary);
+   })
+
+   
+   
 }
 
 window.onload = setup;
